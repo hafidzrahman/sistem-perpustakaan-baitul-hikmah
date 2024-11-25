7341132b1a69f877bcdf1c197d2a8e26c0874913
hzqula <ilooqstrasi@gmail.com>
2024-11-25 15:19:33 +0700

    🎯 chore: change some style

--- app/(petugas perpustakaan)/panel-kontrol/page.tsx
+++ app/(petugas perpustakaan)/panel-kontrol/page.tsx
@@ -23,18 +23,18 @@ const Page = ({}: PageProps) => {
         <CardData number="303" label="Peminjaman" icon={Agreement02Icon} />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 auto-rows-[minmax(180px,_auto)] gap-5">
-        <div className="order-1 col-span-1 p-6 bg-white-custom  rounded-lg border-2 border-border-gray lg:order-none lg:row-span-2 border-gray sm:col-span-2 lg:col-span-3">
+        <div className="order-1 col-span-1 p-6 bg-white-custom  rounded-lg border-2 border-dark-gray lg:order-none lg:row-span-2 dark-gray sm:col-span-2 lg:col-span-3">
           <h1 className="font-source-sans text-2xl text-primary font-bold">
             Ikhtisar
           </h1>
         </div>
 
-        <div className="relative order-4 col-span-1 p-6 bg-white border-2  rounded-lg border-border-gray sm:col-span-2 lg:row-span-2 lg:order-none lg:col-span-1">
+        <div className="relative order-4 col-span-1 p-6 bg-white border-2  rounded-lg border-dark-gray sm:col-span-2 lg:row-span-2 lg:order-none lg:col-span-1">
           <h1 className="font-source-sans text-2xl text-center text-primary font-bold">
             Status Buku
           </h1>
         </div>
-        <div className="flex flex-col max-h-[380px] order-last col-span-1 row-span-2 p-6 overflow-y-auto bg-white  rounded-lg border-2 border-border-gray lg:order-none sm:col-span-2 lg:col-span-4 lg:row-span-2 border-gray">
+        <div className="flex flex-col max-h-[380px] order-last col-span-1 row-span-2 p-6 overflow-y-auto bg-white  rounded-lg border-2 border-dark-gray lg:order-none sm:col-span-2 lg:col-span-4 lg:row-span-2 dark-gray">
           <h1 className="font-source-sans text-2xl text-primary font-bold">
             Riwayat Peminjaman
           </h1>
