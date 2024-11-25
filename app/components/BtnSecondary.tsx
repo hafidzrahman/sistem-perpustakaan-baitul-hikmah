a0e7741c2cfeb2214b6fadc08d9fa81f501440d5
hzqula <ilooqstrasi@gmail.com>
2024-11-25 15:23:49 +0700

    🎯 chore: change some style

--- app/components/BtnSecondary.tsx
+++ app/components/BtnSecondary.tsx
@@ -1,6 +1,6 @@
 interface BtnSecondaryProps {
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   icon?: React.ElementType;
 }
 
@@ -8,8 +8,9 @@ const BtnSecondary = ({ label, onClick, icon: Icon }: BtnSecondaryProps) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-transparent flex justify-between items-center gap-2 text-primary font-source-sans leading-none text-xs font-normal rounded-md border-2 border-primary py-2 px-4 transition-all duration-300
-        hover:bg-primary hover:text-white-custom hover:border-black-custom hover:transition-all hover:duration-300`}
+      className={`bg-transparent flex justify-between items-center gap-2 text-light-primary font-source-sans leading-none text-xs font-normal rounded-md border-2 border-light-primary py-2 transition-all duration-300 hover:font-bold hover:shadow-sm ${
+        Icon ? "pl-6 pr-4" : "pl-4 pr-4"
+      } hover:transition-all hover:duration-300`}
     >
       {label}
       {Icon && <Icon width={20} height={20} />}
