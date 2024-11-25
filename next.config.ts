931c782193f8e4444343eb3f76f563461e6fcd41
hzqula <ilooqstrasi@gmail.com>
2024-11-25 15:10:44 +0700

    🔥 feat: add host domain to upload image

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["books.google.co.id", "gpu.id", "ebooks.gramedia.com"],
+    domains: [
+      "books.google.co.id",
+      "gpu.id",
+      "ebooks.gramedia.com",
+      "s3-ap-southeast-1.amazonaws.com",
+      "image.gramedia.net",
+      "example.com",
+    ],
   },
 };
 
