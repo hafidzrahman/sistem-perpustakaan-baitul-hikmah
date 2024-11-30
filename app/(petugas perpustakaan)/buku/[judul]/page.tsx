a09d6c52378ab866d12f16bbd5a93243160b17d9
hzqula <ilooqstrasi@gmail.com>
2024-11-30 14:45:02 +0700

    🔥 feat: idk what i changed, in real file its hasn't changes

--- app/(petugas perpustakaan)/buku/[judul]/page.tsx
+++ app/(petugas perpustakaan)/buku/[judul]/page.tsx
@@ -26,7 +26,31 @@ const Page = ({ params }: { params: Promise<{ judul: string }> }) => {
   console.log(detailBuku);
 
   if (!detailBuku) {
-    return <div>Bentar</div>;
+    return (
+      <div className="relative bg-gray-200 animate-pulse">
+        <div className="w-full flex gap-4 relative">
+          <div className="w-full h-48 absolute top-0 left-0 bg-gray-200 flex rounded-lg border-dark border-4"></div>
+          <div className="w-1/4 relative z-10 p-12">
+            <div className="rounded-md border-2 w-full border-black-custom h-48 bg-gray-200"></div>
+          </div>
+          <div className="w-3/4 flex flex-col gap-8 z-10 py-12">
+            <div>
+              <div className="h-12 bg-gray-200 rounded w-1/2 mb-1"></div>
+              <div className="h-8 bg-gray-200 rounded w-1/3 mb-2"></div>
+              <div className="flex items-center gap-2 mt-3 h-6 bg-gray-200 rounded w-1/4"></div>
+            </div>
+            <div className="pr-12 flex flex-col gap-2">
+              <div className="h-8 bg-gray-200 rounded w-1/4 mb-2"></div>
+              <div className="text-justify font-source-serif text-sm indent-8">
+                <div className="h-5 bg-gray-200 rounded w-full mt-1"></div>
+                <div className="h-5 bg-gray-200 rounded w-2/3 mt-1"></div>
+                <div className="h-5 bg-gray-200 rounded w-1/2 mt-1"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   const bg = [
