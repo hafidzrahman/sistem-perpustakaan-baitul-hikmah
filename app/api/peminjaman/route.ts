a7c43531d76808979cdb7c4343837308b4476fc5
hzqula <ilooqstrasi@gmail.com>
2024-12-16 06:59:20 +0700

    🎯 chore: change response

--- app/api/peminjaman/route.ts
+++ app/api/peminjaman/route.ts
@@ -5,26 +5,32 @@ import { NextResponse } from "next/server";
 export async function GET() {
   try {
     const dataPeminjaman = await Peminjaman.ambilSemuaDataPeminjaman();
-    await Peminjaman.perbaruiTenggatWaktuPeminjaman(dataPeminjaman[0].id, {isbn : "978-602-06-5192-9", id : 1}, new Date(Date.now() + 10000))
-    const test = await prisma.denda.findMany({})
+    await Peminjaman.perbaruiTenggatWaktuPeminjaman(
+      dataPeminjaman[0].id,
+      { isbn: "978-602-06-5192-9", id: 1 },
+      new Date(Date.now() + 10000)
+    );
 
-    return NextResponse.json({dataPeminjaman, test}, { status: 200 });
+    return NextResponse.json(dataPeminjaman, { status: 200 });
   } catch (error) {
     return NextResponse.json(
-      { message: "Gagal mendapatkan data peminjaman", details : error },
+      { message: "Gagal mendapatkan data peminjaman", details: error },
       { status: 503 }
     );
   }
 }
 
-export async function POST(req : Request) {
+export async function POST(req: Request) {
   try {
     const body = await req.json();
 
     const dataPeminjaman = await Peminjaman.tambahPeminjaman(body);
 
-    return NextResponse.json(dataPeminjaman, {status : 200})
+    return NextResponse.json(dataPeminjaman, { status: 200 });
   } catch (error) {
-    return NextResponse.json({message : "Gagal menambahkan data buku", details : error}, {status : 500})
+    return NextResponse.json(
+      { message: "Gagal menambahkan data buku", details: error },
+      { status: 500 }
+    );
   }
 }
