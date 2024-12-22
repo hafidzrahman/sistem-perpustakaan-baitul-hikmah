549e9a3eebd6e92455bbf9f58103f18374583df7
hzqula <ilooqstrasi@gmail.com>
2024-12-22 13:20:06 +0700

    🔥 feat: responsive

--- app/components/BtnSecondary.tsx
+++ app/components/BtnSecondary.tsx
@@ -8,8 +8,8 @@ const BtnSecondary = ({ label, onClick, icon: Icon }: BtnSecondaryProps) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-transparent flex justify-between items-center gap-2 text-light-primary font-source-sans leading-none text-xs font-normal rounded-md border-2 border-light-primary py-2 transition-all duration-300 hover:font-bold hover:shadow-sm ${
-        Icon ? "pl-6 pr-4" : "pl-4 pr-4"
+      className={`bg-transparent flex justify-between items-center gap-2 text-light-primary font-source-sans text-xs sm:text-sm font-normal rounded-md border-2 border-light-primary py-2 transition-all duration-300 hover:font-bold hover:shadow-sm ${
+        Icon ? "pl-6 pr-4 sm:pl-8 sm:pr-6" : "pl-4 pr-4 sm:pl-6 sm:pr-6"
       } hover:transition-all hover:duration-300`}
     >
       {label}
